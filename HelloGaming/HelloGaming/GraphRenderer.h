312472fc4cc204c4f1cfaaeebea83d264e58dfcc
ShirleyChung <jonesr_tw@yahoo.com.tw>
2016-04-30 16:39:40 +0800

    new graphicrenderer class

---
+++ HelloGaming/HelloGaming/GraphRenderer.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "DirectXBase.h"
+
+// 這個類別會轉譯有彩色背景的簡單文字。
+ref class GraphRenderer sealed : public DirectXBase
+{
+public:
+	GraphRenderer();
+
+	// DirectXBase 方法。
+	virtual void CreateDeviceIndependentResources() override;
+	virtual void CreateDeviceResources() override;
+	virtual void CreateWindowSizeDependentResources() override;
+	virtual void Render() override;
+
+	// 用於更新時間相依物件的方法。
+	void Update(float timeTotal, float timeDelta);
+
+	// 用於根據輸入事件變更文字位置的方法。
+	void UpdateTextPosition(Windows::Foundation::Point deltaTextPosition);
+
+private:
+	Microsoft::WRL::ComPtr<ID2D1SolidColorBrush> m_blackBrush;
+	Microsoft::WRL::ComPtr<IDWriteTextFormat> m_textFormat;
+	Microsoft::WRL::ComPtr<IDWriteTextLayout> m_textLayout;
+	DWRITE_TEXT_METRICS m_textMetrics;
+	Windows::Foundation::Point m_textPosition;
+	bool m_renderNeeded;
+	int m_backgroundColorIndex;
+};
