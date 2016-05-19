4ad6ca8825f603b7eedb6fbfff08f8424e4a983a
ShirleyChung <jonesr_tw@yahoo.com.tw>
2016-05-19 23:05:33 +0800

    add scatterplot

--- HelloGaming/HelloGaming/ScatterPlot.cpp
+++ HelloGaming/HelloGaming/ScatterPlot.cpp
@@ -4,10 +4,10 @@
 
 ScatterPlot::ScatterPlot(float *x, float *y, float nodeSize, D2D1::ColorF nodeColor, NodeShape nodeShape, int count)
 	:GraphVariable(x,y,count)
-	,m_nodeColor(0)
-{
-
-}
+	,m_nodeColor(nodeColor)
+	,m_halfNodeSize(nodeSize/2)
+	,m_nodeShape(nodeShape)
+{}
 
 
 ScatterPlot::~ScatterPlot(void)
@@ -16,10 +16,28 @@ ScatterPlot::~ScatterPlot(void)
 
 void ScatterPlot::CreateDeviceDependentResources(Microsoft::WRL::ComPtr<ID2D1DeviceContext> context)
 {
-
+	DX::ThrowIfFailed( context->CreateSolidColorBrush(D2D1::ColorF(m_nodeColor), &m_brush) );
 }
 
 void ScatterPlot::Render(Microsoft::WRL::ComPtr<ID2D1DeviceContext> context)
 {
-
+	switch(m_nodeShape){
+	case NodeShape::Circle:
+		for(int i=0; i<m_nodeCount; ++i)
+		{
+			context->FillEllipse(D2D1::Ellipse(m_points[i], m_halfNodeSize, m_halfNodeSize), m_brush);
+		}
+		break;
+	case NodeShape::Square:
+		for( int i=0; i<m_nodeCount; ++i)
+		{
+			context->FillRectangle(D2D1::RectF(m_points[i].x - m_halfNodeSize,
+				m_points[i].y - m_halfNodeSize,
+				m_points[i].x + m_halfNodeSize,
+				m_points[i].y + m_halfNodeSize), m_brush);
+		}
+		break;
+	default:
+		break;
+	}
 }
