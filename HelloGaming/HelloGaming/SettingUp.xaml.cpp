c8ebf6558fffc19e7a134915798b6bf1dcff9146
ShirleyChung <jonesr_tw@yahoo.com.tw>
2016-06-07 18:00:02 +0800

    加入setting up page

---
+++ HelloGaming/HelloGaming/SettingUp.xaml.cpp
@@ -0,0 +1,38 @@
+﻿//
+// SettingUp.xaml.cpp
+// SettingUp 類別的實作
+//
+
+#include "pch.h"
+#include "SettingUp.xaml.h"
+
+using namespace HelloGaming;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+
+// 空白頁項目範本已記錄在 http://go.microsoft.com/fwlink/?LinkId=234238
+
+SettingUp::SettingUp(Windows::UI::Xaml::UIElement^ p)
+	:m_parent(p)
+{
+	InitializeComponent();
+}
+
+/// <summary>
+/// 在此頁面即將顯示在框架中時叫用。
+/// </summary>
+/// <param name="e">描述如何到達此頁面的事件資料。Parameter
+/// 屬性通常用來設定頁面。</param>
+void SettingUp::OnNavigatedTo(NavigationEventArgs^ e)
+{
+	(void) e;	// 未使用的參數
+}
---
+++ HelloGaming/HelloGaming/SettingUp.xaml.h
@@ -0,0 +1,26 @@
+﻿//
+// SettingUp.xaml.h
+// SettingUp 類別的宣告
+//
+
+#pragma once
+
+#include "SettingUp.g.h"
+
+namespace HelloGaming
+{
+	/// <summary>
+	/// 可以在本身使用或巡覽至框架內的空白頁面。
+	/// </summary>
+	[Windows::Foundation::Metadata::WebHostHidden]
+	public ref class SettingUp sealed
+	{
+		Windows::UI::Xaml::UIElement^ m_parent;
+
+	public:
+		SettingUp(Windows::UI::Xaml::UIElement^ parent);
+
+	protected:
+		virtual void OnNavigatedTo(Windows::UI::Xaml::Navigation::NavigationEventArgs^ e) override;
+	};
+}
